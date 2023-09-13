0f05255e1bac1db605626fc139fe823bb943b067
Mardyyy <martinapopot@gmail.com>
2023-09-12 22:20:10 -0500

    Added navigation code to the header for multiple pages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-// import logo from './logo.svg';
-import React, { useState } from "react";
+import React from 'react';
 import Header from "../src/components/Header";
-import About from "../src/components/About";
-import Portfolio from "../src/components/Portfolio";
-import Contact from "../src/components/Contact";
-import Resume from "../src/components/Resume";
 import Footer from "../src/components/Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-      <main>
-
-      </main>
-      <footer>
-
-      </footer>
+      <Header className="App-header"></Header>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Contract.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Contract() {
-
-};
-
-export default Contract;
