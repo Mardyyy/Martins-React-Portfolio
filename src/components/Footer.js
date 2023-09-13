75d0692d9489a92d7f2c0ca9378f117ea2e41a68
Mardyyy <martinapopot@gmail.com>
2023-09-13 01:43:43 -0500

    Added content to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Footer() {
-
+    return (
+        <footer>
+            <div>
+                <a href="https://github.com/Mardyyy?tab=repositories">Github</a>
+            </div>
+            <div>
+                <a href="https://github.com/Mardyyy?tab=repositories">LinkedIn...I don't have one</a>
+            </div>
+            <div>
+                <a href="https://www.instagram.com/martinjr_apopot/">Instagram</a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
